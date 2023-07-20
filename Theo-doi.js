e75404d221edcf9356cc7994d56de0a965755913
Nguyenblur <135226678+Nguyenblur@users.noreply.github.com>
2023-07-21 00:40:23 +0700

    Create Theo-doi.js

---
+++ Theo-doi.js
@@ -0,0 +1,106 @@
+<b:if cond='data:view.isPost'>
+var limitBookmark = 100;
+var bookmark = (function(){
+list = [];
+
+//Structure Push to Object New Item
+function Item(id,name,status,type,link,img){
+	this.id = id;
+	this.name = name;
+    this.status = status;
+	this.type = type;
+    this.link = link;
+	this.img = img;
+}
+
+//Event Saving to Local Storage
+function setBookmark(){
+	localStorage.setItem('bookmark', JSON.stringify(list));
+}
+
+function loadBookmark() {
+    list = JSON.parse(localStorage.getItem('bookmark'));
+}
+
+if (localStorage.getItem("bookmark") != null) {
+    loadBookmark();
+}
+
+obj = {};
+//Add New Item Object to Array
+obj.addItemTobookmark = function(id,name,status,type,link,img) {
+    var item = new Item(id,name,status,type,link,img),
+    itemList = list;
+    if(itemList != null){
+    same = itemList.find(item =>{return item.id == id;});
+    if(list.length<limitBookmark){
+     if(!same){
+    	list.push(item);
+    	setBookmark();
+      }
+     }
+    }else{
+    	list.push(item);
+    	setBookmark();
+    }
+}
+
+//Remove Bookmark    
+obj.removeThisItem = function(id) {
+    for(var item in list) {
+      if(list[item].id === id) {
+        list.splice(item, 1);
+        break;
+      }
+    }
+    setBookmark();
+  }
+  
+  return obj;
+})();
+
+$('.bookmark').each(function(event) {
+const getData = JSON.parse(localStorage.getItem('bookmark'));
+for(var i in getData){
+	if(getData[i].id == $(this).data('id')){
+     $(this).html('Bookmarked')
+     $(this).addClass('bookmarked')
+    }
+}
+  $(this).click(function(){
+const list = JSON.parse(localStorage.getItem('bookmark'));
+  //Retrieve Data From Post
+  	const id = $(this).data('id'),
+  	name = $('article.oh.a2 header h1.mb-6').text().replace('\n',''),
+    link = location.protocol + '//' + location.hostname +  location.pathname,
+    img = $('div.grid div.a1 figure img').attr('src'),
+    status = $('aside.s1 div.y6x11p:nth-child(1) span.dt a').text().replace('\n',''),
+    type = $('aside.s1 div.y6x11p:nth-child(2) span.dt a').text().replace('\n','');
+    
+  //Set To Function Bookmark
+if(list == null){
+  if(!$(this).hasClass('bookmarked')){
+    	bookmark.addItemTobookmark(id,name,status,type,link,img);
+  		$(this).addClass('bookmarked')
+  		$(this).html('Bookmarked')
+  }else{
+  	bookmark.removeThisItem(id);
+  	$(this).html('Bookmark <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" d="M1 3.25C1 2.56 1.56 2 2.249 2h.5c.69 0 1.248.56 1.248 1.25v9.495c0 .69-.559 1.25-1.248 1.25h-.5A1.25 1.25 0 0 1 1 12.744V3.249ZM2.249 3a.25.25 0 0 0-.25.25v9.495c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25V3.249a.25.25 0 0 0-.25-.25h-.5Zm2.748.25c0-.69.559-1.25 1.249-1.25h.5c.689 0 1.248.56 1.248 1.25v9.495c0 .69-.56 1.25-1.249 1.25h-.5a1.25 1.25 0 0 1-1.248-1.25V3.249ZM6.246 3a.25.25 0 0 0-.25.25v9.495c0 .138.112.25.25.25h.5a.25.25 0 0 0 .249-.25V3.249a.25.25 0 0 0-.25-.25h-.5Zm5.726 1.777a1.249 1.249 0 0 0-1.57-.713l-.583.204a1.25 1.25 0 0 0-.746 1.645l2.937 7.304c.249.62.94.933 1.571.713l.582-.204a1.25 1.25 0 0 0 .746-1.646l-2.937-7.303Zm-1.24.23a.25.25 0 0 1 .313.143l2.937 7.303a.25.25 0 0 1-.149.33l-.582.203a.25.25 0 0 1-.314-.142L10 5.54a.25.25 0 0 1 .149-.329l.582-.204Z"/></svg>')
+  	$(this).removeClass('bookmarked')
+  }
+}else{
+  if(!$(this).hasClass('bookmarked')){
+if(list.length<limitBookmark){
+    	bookmark.addItemTobookmark(id,name,status,type,link,img);
+  		$(this).addClass('bookmarked')
+  		$(this).html('Bookmarked')
+}
+  }else{
+  	bookmark.removeThisItem(id);
+  	$(this).html('Bookmark <svg xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="img" width="1em" height="1em" preserveAspectRatio="xMidYMid meet" viewBox="0 0 16 16"><path fill="currentColor" d="M1 3.25C1 2.56 1.56 2 2.249 2h.5c.69 0 1.248.56 1.248 1.25v9.495c0 .69-.559 1.25-1.248 1.25h-.5A1.25 1.25 0 0 1 1 12.744V3.249ZM2.249 3a.25.25 0 0 0-.25.25v9.495c0 .138.112.25.25.25h.5a.25.25 0 0 0 .25-.25V3.249a.25.25 0 0 0-.25-.25h-.5Zm2.748.25c0-.69.559-1.25 1.249-1.25h.5c.689 0 1.248.56 1.248 1.25v9.495c0 .69-.56 1.25-1.249 1.25h-.5a1.25 1.25 0 0 1-1.248-1.25V3.249ZM6.246 3a.25.25 0 0 0-.25.25v9.495c0 .138.112.25.25.25h.5a.25.25 0 0 0 .249-.25V3.249a.25.25 0 0 0-.25-.25h-.5Zm5.726 1.777a1.249 1.249 0 0 0-1.57-.713l-.583.204a1.25 1.25 0 0 0-.746 1.645l2.937 7.304c.249.62.94.933 1.571.713l.582-.204a1.25 1.25 0 0 0 .746-1.646l-2.937-7.303Zm-1.24.23a.25.25 0 0 1 .313.143l2.937 7.303a.25.25 0 0 1-.149.33l-.582.203a.25.25 0 0 1-.314-.142L10 5.54a.25.25 0 0 1 .149-.329l.582-.204Z"/></svg>')
+  	$(this).removeClass('bookmarked')
+  }
+}
+  })
+});
+</b:if>
